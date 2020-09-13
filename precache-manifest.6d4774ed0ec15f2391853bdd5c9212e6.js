c1764fb42361eea25252072dd263dce08a891646
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-13 19:18:03 +0300

    Updates

---
+++ precache-manifest.6d4774ed0ec15f2391853bdd5c9212e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f94572c1350d92e1656224d3c539ef",
+    "url": "/box-maker/index.html"
+  },
+  {
+    "revision": "f4c32af98e7d6dcf09a1",
+    "url": "/box-maker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08b4fbfb92d2ca0cd9c5",
+    "url": "/box-maker/static/js/2.728feacd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/box-maker/static/js/2.728feacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c32af98e7d6dcf09a1",
+    "url": "/box-maker/static/js/main.a25a6dd3.chunk.js"
+  },
+  {
+    "revision": "91f4ea729812b8acd6ec",
+    "url": "/box-maker/static/js/runtime-main.bcbcb8e7.js"
+  }
+]);
